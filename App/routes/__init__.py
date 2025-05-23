1a5933fbb5043a81b7eca4e7a9ee4b2d035c2b04
benL25 <148346195+benL25@users.noreply.github.com>
2025-05-22 20:56:51 -0400

    Working css

--- App/routes/__init__.py
+++ App/routes/__init__.py
@@ -2,7 +2,11 @@
 from flask import Blueprint
 import os
 
-main = Blueprint('main', __name__, template_folder=os.path.abspath(os.path.join(os.path.dirname(__file__), '../../templates')))
+main = Blueprint('main', __name__,
+    template_folder=os.path.abspath(os.path.join(os.path.dirname(__file__), '../../templates')),
+    static_folder="/Users/benlozzano/VS-Code-Coding/Ongoing/benMaxTutoring/static",
+    static_url_path="/main_static")
+
 
 # Import and register all route modules here
 from . import user_routes, session_routes, admin_routes, file_routes, notification_routes, misc_routes
